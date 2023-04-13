e0d06ab1e54b1a087dc112c1518c33066b1ffa6c
Unoalgot <130466893+Unoalgot@users.noreply.github.com>
2023-04-13 14:43:55 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,3 +1,9 @@
 $("#quiz-form").submit(function () {
 return false;
+function getCharacter();
 });
+
+function getCharacter() {
+  respectMotionPreference(document.querySelector("#results"));
+  fadeOut(document.querySelector("#submit"));
+  fadeIn(document.querySelector(".loading-wrapper"));
