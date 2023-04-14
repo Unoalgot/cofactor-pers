12a0cee2d49561e03d68f937e2428d99fc12bf0b
Unoalgot <130466893+Unoalgot@users.noreply.github.com>
2023-04-14 10:55:54 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -171,6 +171,7 @@ function getCharacter() {
       document.querySelector("#resultDescription").innerText =
         data.fields.resultDescription;
       document.querySelector("#resultImage").src = data.fields.resultImage;
+      document.querySelector("#resultLink").src = data.fields.resultLink;
     })
        .catch(function writeError(err) {
       console.log(err);
